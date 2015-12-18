1d484db548cae4de806c343a47da01bf23160400
bokapaatha <boka.paatha@gmail.com>
2015-12-19 01:27:19 +0530

    Just testing out raw vanilla js.

---
+++ Helloworld.js
@@ -0,0 +1,12 @@
+// This is a remark.
+alert('Hello world.'); 
+
+// This is a bit further. There is a variable. 
+var count = 1 ; 
+alert ("The count is " + count ) ;
+
+// Being able to log is some consolation. 
+console.log("There is no consolation.") ; 
+
+// Now for some DOM modification by JS. 
+document.getElementById("say-something").innerHTML = "Hello JavaScript";
